0f4c502130b177e6a12471ab02dca79735004ae6
Silmara Ribeiro <silmara.ribeiro@gmail.com>
2020-09-13 11:55:50 -0300

    feat: news feed

--- safra-connect/src/app/news/news.page.ts
+++ safra-connect/src/app/news/news.page.ts
@@ -7,6 +7,61 @@ import { Component } from '@angular/core';
 })
 export class NewsPage {
 
+    noticias = [
+      {
+        nome: 'Ana Maria',
+        texto: 'Essa é a primeira notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'João Pedro',
+        texto: 'Essa é a segunda notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Sophia Bruna',
+        texto: 'Essa é a terceira notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Heitor João',
+        texto: 'Essa é a quarta notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Gustavo César',
+        texto: 'Essa é a quinta notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Ana Júlia',
+        texto: 'Essa é a sexta notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Pedro Henrique',
+        texto: 'Essa é a sétima notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Paulo Henrique',
+        texto: 'Essa é a oitava notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Sabrina Alice',
+        texto: 'Essa é a nona notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      },
+      {
+        nome: 'Bruna Lara',
+        texto: 'Essa é a décima notícia do aplicativo Safra Connect!',
+        foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+      }
+    ];
+
   constructor() {}
+
+  ionViewWillEnter() {}
 
 }

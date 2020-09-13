70d3d67f0c995169b6797dae3797ce80d267175f
Silmara Ribeiro <silmara.ribeiro@gmail.com>
2020-09-13 14:43:39 -0300

    feat: shared expenses list

--- safra-connect/src/app/shared-expenses/shared-expenses.page.ts
+++ safra-connect/src/app/shared-expenses/shared-expenses.page.ts
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class SharedExpensesPage {
 
+  rateios = [
+    {
+      nome: 'Rateio da viagem',
+      inicio: '13/09/2020',
+      situacao: 'Concluído',
+      foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+    },
+    {
+      nome: 'Rateio do churrasco',
+      inicio: '13/09/2020',
+      situacao: 'Fechado',
+      foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+    },
+    {
+      nome: 'Divisão do aluguel',
+      inicio: '13/09/2020',
+      situacao: 'Em andamento',
+      foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+    },
+    {
+      nome: 'Grupo do aniversário',
+      inicio: '13/09/2020',
+      situacao: 'Concluído',
+      foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+    },
+    {
+      nome: 'Rateio da festinha',
+      inicio: '13/09/2020',
+      situacao: 'Concluído',
+      foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+    },
+    {
+      nome: 'Rateio da Netflix',
+      inicio: '13/09/2020',
+      situacao: 'Concluído',
+      foto: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y'
+    }
+  ];
+
   constructor() {}
 
 }
